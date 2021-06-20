88ff454c1e253a1d360bfb1309381b7bb557b7e7
Naveen <lingamna@adobe.com>
2021-06-20 20:54:59 +0530

    elevate automation

---
+++ src/main/java/test/elevate/APITest/WeatherAPIMainTest.java
@@ -0,0 +1,18 @@
+package test.elevate.APITest;
+
+import test.elevate.web.utility.APIHelper;
+import test.elevate.web.utility.CommonUtils;
+import test.elevate.web.utility.StatusChecker;
+import test.elevate.web.utility.Utils;
+
+public class WeatherAPIMainTest {
+	APIHelper helper = new APIHelper();
+	Utils utils = new Utils();
+	public StatusChecker statusChecker = new StatusChecker();
+	protected static CommonUtils commonUtils = new CommonUtils();
+
+	public WeatherAPIMainTest() {
+		super();
+	}
+
+}
---
+++ src/main/java/test/elevate/web/utility/WebDriverConfig.java
@@ -0,0 +1,23 @@
+package test.elevate.web.utility;
+
+import java.io.IOException;
+
+import test.elevate.web.constants.WebDriverConstants;
+
+public class WebDriverConfig {
+	public static boolean usingProxyServer() {
+		return (Boolean.valueOf(System.getProperty(WebDriverConstants.PROXY_SERVER, "false")));
+	}
+
+	public static boolean usingGrid() {
+		return Boolean.parseBoolean(System.getProperty(WebDriverConstants.GRID_SERVER, "false"));
+	}
+
+	public static String getWebDriverProperty(String propertyName) throws IOException {
+		if (propertyName.equalsIgnoreCase("gridserver"))
+			return "localhost";
+		else if (propertyName.equalsIgnoreCase("gridserverport"))
+			return "4444";
+		return null;
+	}
+}
